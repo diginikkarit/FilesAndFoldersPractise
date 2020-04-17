cb9d8b118d43a0050b5fde75d39ddf38b112c4e7
mustohe <hmustone18@student.oulu.fi>
2020-04-17 07:22:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesAndFoldersPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
